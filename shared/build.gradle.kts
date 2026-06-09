2103b13c994889c1d96b62c9c284241a001be8a6
keisary <keisarynakamura@gmail.com>
2026-06-09 21:50:52 +0000

    Initial commit : Structure Compose Multiplatform de jetBrain pour KeycePass

---
+++ shared/build.gradle.kts
@@ -0,0 +1,66 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kmp.library)
+    alias(libs.plugins.maven.publish)
+}
+
+kotlin {
+    android {
+        namespace = "com.ak.keycepass.shared"
+        compileSdk = 36
+        minSdk = 23
+        androidResources.enable = true
+        compilerOptions { jvmTarget = JvmTarget.JVM_17 }
+    }
+
+    jvm {
+        compilerOptions { jvmTarget = JvmTarget.JVM_17 }
+    }
+    wasmJs { browser() }
+    mingwX64()
+
+    sourceSets {
+        commonMain.dependencies {
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+
+    }
+}
+
+//Publishing your Kotlin Multiplatform library to Maven Central
+//https://www.jetbrains.com/help/kotlin-multiplatform-dev/multiplatform-publish-libraries.html
+mavenPublishing {
+    publishToMavenCentral()
+    coordinates("com.ak.keycepass", "shared", "1.0.0")
+
+    pom {
+        name = "KeycePass"
+        description = "Kotlin Multiplatform library"
+        url = "github url" //todo
+
+        licenses {
+            license {
+                name = "MIT"
+                url = "https://opensource.org/licenses/MIT"
+            }
+        }
+
+        developers {
+            developer {
+                id = "" //todo github nickname
+                name = "" //todo full name
+                email = "" //todo email
+            }
+        }
+
+        scm {
+            url = "github url" //todo
+        }
+    }
+    if (project.hasProperty("signing.keyId")) signAllPublications()
+}
