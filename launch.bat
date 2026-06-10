8cfbfe314d24898756cb5158a64e11bc5e6f1fe5
keisary <keisarynakamura@gmail.com>
2026-06-10 15:59:50 +0000

    correction approter au script de lancement et de build de l'app dektop

--- launch.bat
+++ launch.bat
@@ -6,12 +6,8 @@ echo   KeycePass - Administration des Presences
 echo =========================================
 echo.
 
-set "JAVA_HOME=C:\Program Files\Microsoft\jdk-21.0.11.10-hotspot"
-set "LIB_DIR=C:\Users\jules\Desktop\stack\controle-presence\KeycePass\desktopApp\build\install\desktopApp\lib"
-set "MAIN=com.ak.keycepass.desktop.MainKt"
-
 echo Lancement (backend + interface)...
-"%JAVA_HOME%\bin\javaw" -Dskiko.renderApi=SOFTWARE_FAST -classpath "%LIB_DIR%\*" %MAIN%
+call gradlew.bat :desktopApp:run
 
 echo.
 echo App fermee. Double-cliquez pour relancer.
--- launch_console.bat
+++ launch_console.bat
@@ -6,13 +6,9 @@ echo   KeycePass - Presence / Badging Desktop
 echo =========================================
 echo.
 
-set "JAVA_HOME=C:\Program Files\Microsoft\jdk-21.0.11.10-hotspot"
-set "LIB_DIR=C:\Users\jules\Desktop\stack\controle-presence\KeycePass\desktopApp\build\install\desktopApp\lib"
-set "MAIN=com.ak.keycepass.desktop.MainKt"
-
 echo Lancement (mode console - logs visibles)...
 echo.
-"%JAVA_HOME%\bin\java" -Dskiko.renderApi=SOFTWARE_FAST -classpath "%LIB_DIR%\*" %MAIN%
+call gradlew.bat :desktopApp:run
 
 echo.
 echo App fermee.
