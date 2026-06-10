f883f41142e6a10a3cefbb508e57b7e5d29c758a
UnoriginalDude14 <atayiuriel014@gmail.com>
2026-06-10 16:59:37 +0000

    android(logic): cree l'entite EtudiantLocal pour Room

---
+++ androidApp/src/main/kotlin/com/ak/keycepass/android/data/local/entities/EtudiantLocal.kt
@@ -0,0 +1,12 @@
+package com.ak.keycepass.android.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "etudiant_local")
+data class EtudiantLocal(
+    @PrimaryKey val matricule: String,
+    val identifiantClasse: String,
+    val identifiantAppareil: String,
+    val role: String  // ETUDIANT / DELEGUE / ENSEIGNANT
+)
