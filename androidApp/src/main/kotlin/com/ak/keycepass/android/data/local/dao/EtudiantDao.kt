236b79549adb804eeeec499cb9bd6662196e589e
UnoriginalDude14 <atayiuriel014@gmail.com>
2026-06-10 17:03:42 +0000

    android(logic): ajout incremental de androidApp/src/main/kotlin/com/ak/keycepass/android/data/local/dao/EtudiantDao.kt

---
+++ androidApp/src/main/kotlin/com/ak/keycepass/android/data/local/dao/EtudiantDao.kt
@@ -0,0 +1,17 @@
+package com.ak.keycepass.android.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.ak.keycepass.android.data.local.entities.EtudiantLocal
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AccesEtudiant {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun inserer(etudiant: EtudiantLocal)
+
+    @Query("SELECT * FROM etudiant_local WHERE matricule = :matricule LIMIT 1")
+    suspend fun trouverParMatricule(matricule: String): EtudiantLocal?
+}
