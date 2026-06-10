0bb181f24621e203c08017a142b5b5d3d9da5935
UnoriginalDude14 <atayiuriel014@gmail.com>
2026-06-10 12:25:23 +0000

    feat(ui): define StateSuccess, StateLate and StateAbsent colors in Color.kt

--- androidApp/src/main/kotlin/com/ak/keycepass/android/ui/theme/Color.kt
+++ androidApp/src/main/kotlin/com/ak/keycepass/android/ui/theme/Color.kt
@@ -1,1 +1,15 @@
 package com.ak.keycepass.android.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val StateSuccess = Color(0xFF10B981) // Green
+val StateLate = Color(0xFFF59E0B) // Amber
+val StateAbsent = Color(0xFFEF4444) // Red
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
