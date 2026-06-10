a9cb4e613f8c668a4263ea1830e4f156b6a2c9f9
Japhet1705 <tamatekoujaphet0@gmail.com>
2026-06-10 16:53:00 +0000

    Mon fisrt commit

---
+++ desktopApp/src/main/kotlin/com/ak/keycepass/desktop/data/database/schema .sql
@@ -0,0 +1,9 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE IF NOT EXISTS "Enseignant" (
+    "id_enseignant" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "matricule_enseignant" VARCHAR(50) NOT NULL UNIQUE,
+    "nom" VARCHAR(100) NOT NULL,
+    "prenom" VARCHAR(100) NOT NULL,
+    "device_uuid" VARCHAR(255) UNIQUE NULL
+);
